0730909a0f3c8461b68766db37a4fa08c5cf0308
Vitor Nascimento <bielvitor2008@hotmail.com>
2024-02-26 22:24:18 -0300

    refactor: update NoteEntity PrimaryKey

--- app/src/main/java/me/vitornascimento/mynotes/data/adapter/NoteEntity.kt
+++ app/src/main/java/me/vitornascimento/mynotes/data/adapter/NoteEntity.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "notes")
 data class NoteEntity(
-    @PrimaryKey(autoGenerate = true) val id: Long = -1,
+    @PrimaryKey(autoGenerate = true) val id: Long? = null,
     val title: String,
     val content: String,
 )
